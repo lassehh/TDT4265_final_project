5856fc855642e047a6d19ba43465e5f8b2c123e6
lassehh <lassehhenriksen@gmail.com>
2017-04-18 16:12:56 +0200

    First Convnet for traffic sign recognition compiled and saved as .h5

--- src/traffic_sign_recognition/neural_network/load_traffic_signs.py
+++ src/traffic_sign_recognition/neural_network/load_traffic_signs.py
@@ -1,5 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
 
 # This module only works for the specific case of reading traffic signs and is not
 # intended as a general file reading module.
@@ -8,26 +11,34 @@ import matplotlib.pyplot as plt
 def training(rootpath):
     images = []
     labels = []
-    for c in range(0,1):
+    i = 0
+    for c in range(0,43):
         prefix = rootpath + '/' + format(c, '05d') + '/'
         gtFile = open(prefix + 'GT-' + format(c, '05d') + '.csv')
         gtReader = csv.reader(gtFile, delimiter=';')
         gtReader.__next__()
         for row in gtReader:
-            images.append(plt.imread(prefix + row[0]))
+            images.append(Image.open(prefix + row[0]))
+            images[i] = np.array(images[i].resize((90, 90), Image.ANTIALIAS))
             labels.append(row[7])
+            i += 1
+            print('Training images loaded: ', i)
         gtFile.close()
     return images, labels
 
 def testing(rootpath):
     images = []
     labels = []
+    i = 0
     prefix = rootpath + '/'
     gtFile = open(prefix + 'GT-final_test' + '.csv')
     gtReader = csv.reader(gtFile, delimiter=';')
     gtReader.__next__()
     for row in gtReader:
-        images.append(plt.imread(prefix + row[0]))
+        images.append(Image.open(prefix + row[0]))
+        images[i] = np.array(images[i].resize((90, 90), Image.ANTIALIAS))
         labels.append(row[7])
+        i += 1
+        print('Testing images loaded: ', i)
     gtFile.close()
     return images, labels
--- src/traffic_sign_recognition/neural_network/traffic_signs_simple_cnn.py
+++ src/traffic_sign_recognition/neural_network/traffic_signs_simple_cnn.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import csv
+from PIL import Image
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -16,19 +17,16 @@ seed = 7
 np.random.seed(seed)
 
 # read images
-[X_train, Y_train] = load_traffic_signs.training('GTSRB/Final_Training/Images')
+print('Loading images...')
 [X_test, Y_test] = load_traffic_signs.testing('GTSRB/Final_Testing/Images')
-print(X_train[0])
-# format, normalize
-X_train = Image.fromarray(X_train)
-X_train = X_train.resize((90,90,), Image.HAMMING)
-X_test = Image.fromarray(X_test)
-X_test = X_test.resize((90,90), Image.HAMMING)
+[X_train, Y_train] = load_traffic_signs.training('GTSRB/Final_Training/Images')
+print('Done!')
 
+# format, normalize
+print('Formating and normalizing images...')
 X_train = np.asarray(X_train)
 X_test = np.asarray(X_test)
 
-print(X_train.shape)
 
 X_train = X_train.reshape(X_train.shape[0], 3, 90, 90).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], 3, 90, 90).astype('float32')
@@ -39,15 +37,11 @@ X_test = X_test / 255
 Y_train = np_utils.to_categorical(Y_train)
 Y_test = np_utils.to_categorical(Y_test)
 num_classes = Y_test.shape[1]
-
-# show a single image
-plt.imshow(X_train[22])
-print(X_train[22][0][0])
-plt.show()
+print('Done!')
 
 def baseline_model():
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(1, 28, 28)))
+    model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(3, 90, 90)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
     model.add(Flatten())
